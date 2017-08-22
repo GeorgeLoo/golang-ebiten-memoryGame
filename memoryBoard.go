6bbb082a444276bc33d0189711665342669e4c57
Hajime Hoshi <hajimehoshi@gmail.com>
2017-08-23 02:21:36 +0900

    gofmt -s -w .

--- memoryBoard.go
+++ memoryBoard.go
@@ -1,5 +1,3 @@
-
-
 /*
 
 memory.go  13.8.2017
@@ -19,78 +17,74 @@ hide
 
 package main
 
-
 import (
-    //"log"
-    "time"
-    "reflect"
-    "math/rand"
-    "fmt"
-    "image/color"
-    "github.com/hajimehoshi/ebiten"
-    "github.com/hajimehoshi/ebiten/ebitenutil"
+	//"log"
+	"fmt"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image/color"
+	"math/rand"
+	"reflect"
+	"time"
 )
 
 const (
-    MG_matched = 100
-    oneWidth = 30
-    oneHeight = 30
-    oneGap = 7
-    numRows = 8
-    numCols = 8
-    numCards = numRows * numCols
-    noCardClicked = -999
-    numFramesPerSec = 60
-    numSecondsToShowAll = 2 * numFramesPerSec
-    numSecondsToShowTwo = 0.25 * numFramesPerSec
-    numMatching = 2
-    numColors = 7
-    gameCompleteMatches = (numCols * numRows) / 2
-)   
+	MG_matched          = 100
+	oneWidth            = 30
+	oneHeight           = 30
+	oneGap              = 7
+	numRows             = 8
+	numCols             = 8
+	numCards            = numRows * numCols
+	noCardClicked       = -999
+	numFramesPerSec     = 60
+	numSecondsToShowAll = 2 * numFramesPerSec
+	numSecondsToShowTwo = 0.25 * numFramesPerSec
+	numMatching         = 2
+	numColors           = 7
+	gameCompleteMatches = (numCols * numRows) / 2
+)
 
 type cardObjectData struct {
-    //name string
-    colorCode int        
-    x,y,w,h float64
-    clicked bool
+	//name string
+	colorCode  int
+	x, y, w, h float64
+	clicked    bool
 }
 
 type matchingData struct {
-    colorCode int
-    squareIdx int
+	colorCode int
+	squareIdx int
 }
 
 // type cardGraphicsData struct {
 //     image *ebiten.Image
 // }
 
-
 var (
-    aCardData cardObjectData 
-    cardArr []cardObjectData
-	cardRectImage *ebiten.Image
-    frontCardImage *ebiten.Image
-    show bool
-    frameCount int
-    timerLockShowAll bool
-    matcher [10]matchingData  // hack! 10 to cater for bug!
-    matcherIdx int
-    ShowTwoTimerLock bool
-    tries int
-    matches int
-    backToMain int
-    hgameover bool
-
+	aCardData        cardObjectData
+	cardArr          []cardObjectData
+	cardRectImage    *ebiten.Image
+	frontCardImage   *ebiten.Image
+	show             bool
+	frameCount       int
+	timerLockShowAll bool
+	matcher          [10]matchingData // hack! 10 to cater for bug!
+	matcherIdx       int
+	ShowTwoTimerLock bool
+	tries            int
+	matches          int
+	backToMain       int
+	hgameover        bool
 )
 
-
 func initMatcher() {
-    fmt.Printf("initMatcher \n")
-    matcherIdx = 0
-    for i := 0; i < numMatching; i++ {
-        matcher[i].colorCode = -1
-        matcher[i].squareIdx = -1
-    }
+	fmt.Printf("initMatcher \n")
+	matcherIdx = 0
+	for i := 0; i < numMatching; i++ {
+		matcher[i].colorCode = -1
+		matcher[i].squareIdx = -1
+	}
 }
 
 /*
@@ -112,271 +106,263 @@ clicked on a square
 */
 func mG_Click(mx, my int) bool {
 
-    var x, y, w, h, x1, y1 int
-     
+	var x, y, w, h, x1, y1 int
 
-    m := len(cardArr)
-    for i := 0; i < m; i++ {
-        x = int (cardArr[i].x)
-        y = int (cardArr[i].y)
-        w = int (cardArr[i].w)
-        h = int (cardArr[i].h)
-        x1 = x + w
-        y1 = y + h
-        if mx > x && mx < x1 && my > y && my < y1 {
-            if ! cardArr[i].clicked && matcherIdx < 2 {
-                fmt.Printf("card %d clicked\n", i)
-                matcher[matcherIdx].colorCode = cardArr[i].colorCode
-                matcher[matcherIdx].squareIdx = i
-                fmt.Printf("matcherIdx %d \n", matcherIdx)
-                if matcherIdx < 2 {
-                    matcherIdx += 1
-                }
-                cardArr[i].clicked = ! cardArr[i].clicked
-                if matcherIdx > 1 {
-                    fmt.Printf("2 cards clicked\n")
-                    fmt.Printf("matcherIdx %d \n", matcherIdx)
-                    ShowTwoTimerLock = false
-                }
+	m := len(cardArr)
+	for i := 0; i < m; i++ {
+		x = int(cardArr[i].x)
+		y = int(cardArr[i].y)
+		w = int(cardArr[i].w)
+		h = int(cardArr[i].h)
+		x1 = x + w
+		y1 = y + h
+		if mx > x && mx < x1 && my > y && my < y1 {
+			if !cardArr[i].clicked && matcherIdx < 2 {
+				fmt.Printf("card %d clicked\n", i)
+				matcher[matcherIdx].colorCode = cardArr[i].colorCode
+				matcher[matcherIdx].squareIdx = i
+				fmt.Printf("matcherIdx %d \n", matcherIdx)
+				if matcherIdx < 2 {
+					matcherIdx += 1
+				}
+				cardArr[i].clicked = !cardArr[i].clicked
+				if matcherIdx > 1 {
+					fmt.Printf("2 cards clicked\n")
+					fmt.Printf("matcherIdx %d \n", matcherIdx)
+					ShowTwoTimerLock = false
+				}
 
-            }
-        }
-    }
+			}
+		}
+	}
 
-    if hgameover {
-        backToMain++
-        fmt.Printf("backToMain %d \n", backToMain)
-        if backToMain > 0 {
-            mouseReleased = false
-            return true
-        }
-    }
+	if hgameover {
+		backToMain++
+		fmt.Printf("backToMain %d \n", backToMain)
+		if backToMain > 0 {
+			mouseReleased = false
+			return true
+		}
+	}
 
-    return false
+	return false
 }
 
 func checkIfMatch() {
-    var j int
-    fmt.Printf("checkIfMatch \n")
-    if matcher[0].colorCode != matcher[1].colorCode {
-        fmt.Printf("not matching cards clicked\n")
-        j = matcher[0].squareIdx
-        fmt.Printf("j %d \n", j)
-        cardArr[j].clicked = false
-        j = matcher[1].squareIdx
-        fmt.Printf("j %d \n", j)
-        cardArr[j].clicked = false
-        tries++
-    } else {
-        tries++
-        matches++
-    }
-    initMatcher()
+	var j int
+	fmt.Printf("checkIfMatch \n")
+	if matcher[0].colorCode != matcher[1].colorCode {
+		fmt.Printf("not matching cards clicked\n")
+		j = matcher[0].squareIdx
+		fmt.Printf("j %d \n", j)
+		cardArr[j].clicked = false
+		j = matcher[1].squareIdx
+		fmt.Printf("j %d \n", j)
+		cardArr[j].clicked = false
+		tries++
+	} else {
+		tries++
+		matches++
+	}
+	initMatcher()
 }
 
-func getColorCode( c int) color.Color {
-    switch c {
-    case 0:
-        return color.NRGBA{255, 0, 0, 0xff}
-    case 1:
-        return color.NRGBA{0, 255, 0, 0xff}
-    case 2:
-        return color.NRGBA{0, 0, 255, 0xff}
-    case 3:
-        return color.NRGBA{250, 250, 250, 0xff}
-    case 4:
-        return color.NRGBA{255, 255, 0, 0xff}  // yellow
-    case 5:
-        return color.NRGBA{128, 128, 128, 0xff}  // gray
-    case 6:
-        return color.NRGBA{128, 0, 255, 0xff} // purple
-    default:
-        return color.NRGBA{0xff, 0xff, 0xff, 0xff}
-    }
+func getColorCode(c int) color.Color {
+	switch c {
+	case 0:
+		return color.NRGBA{255, 0, 0, 0xff}
+	case 1:
+		return color.NRGBA{0, 255, 0, 0xff}
+	case 2:
+		return color.NRGBA{0, 0, 255, 0xff}
+	case 3:
+		return color.NRGBA{250, 250, 250, 0xff}
+	case 4:
+		return color.NRGBA{255, 255, 0, 0xff} // yellow
+	case 5:
+		return color.NRGBA{128, 128, 128, 0xff} // gray
+	case 6:
+		return color.NRGBA{128, 0, 255, 0xff} // purple
+	default:
+		return color.NRGBA{0xff, 0xff, 0xff, 0xff}
+	}
 
 }
 
 func show_all() {
 
-    for i := 0; i < numCards; i++ {
-        cardArr[i].clicked = true
-    }
+	for i := 0; i < numCards; i++ {
+		cardArr[i].clicked = true
+	}
 }
 func cover_all() {
-    for i := 0; i < numCards; i++ {
-        cardArr[i].clicked = false
-    }
+	for i := 0; i < numCards; i++ {
+		cardArr[i].clicked = false
+	}
 }
 
 func MG_Draw(screen *ebiten.Image) {
-    var msg string
-    // Create an empty option struct
-    opts := &ebiten.DrawImageOptions{}
-    for i := 0; i < numCards; i++ {
-        x := cardArr[i].x
-        y := cardArr[i].y
-        opts.GeoM.Reset()
-        opts.GeoM.Scale( 1.0, 1.0 )
-        opts.GeoM.Translate(x, y)
-        if cardArr[i].clicked {
-            frontCardImage.Fill(getColorCode(cardArr[i].colorCode))
-            screen.DrawImage(frontCardImage, opts)
-        } else {
-            screen.DrawImage(cardRectImage, opts)
+	var msg string
+	// Create an empty option struct
+	opts := &ebiten.DrawImageOptions{}
+	for i := 0; i < numCards; i++ {
+		x := cardArr[i].x
+		y := cardArr[i].y
+		opts.GeoM.Reset()
+		opts.GeoM.Scale(1.0, 1.0)
+		opts.GeoM.Translate(x, y)
+		if cardArr[i].clicked {
+			frontCardImage.Fill(getColorCode(cardArr[i].colorCode))
+			screen.DrawImage(frontCardImage, opts)
+		} else {
+			screen.DrawImage(cardRectImage, opts)
 
-        }
+		}
 
-    }
+	}
 
-    if ! timerLockShowAll {
-        frameCount += 1
-        if frameCount > numSecondsToShowAll {
-            timerLockShowAll = true
-            cover_all()
-            frameCount = 0
-        }
-    }
+	if !timerLockShowAll {
+		frameCount += 1
+		if frameCount > numSecondsToShowAll {
+			timerLockShowAll = true
+			cover_all()
+			frameCount = 0
+		}
+	}
 
-    if ! ShowTwoTimerLock {
-        frameCount += 1
-        if frameCount > numSecondsToShowTwo {
-            ShowTwoTimerLock = true
-            frameCount = 0
-            checkIfMatch()
-        }
+	if !ShowTwoTimerLock {
+		frameCount += 1
+		if frameCount > numSecondsToShowTwo {
+			ShowTwoTimerLock = true
+			frameCount = 0
+			checkIfMatch()
+		}
 
-    }
+	}
 
-    if matches == gameCompleteMatches {
-        msg = fmt.Sprintf(" GAME OVER ...Tries %d Matches %d",tries,matches)
-        hgameover = true
-    } else {
-        msg = fmt.Sprintf(" Tries %d Matches %d",tries,matches)
-    }
-    
-    ebitenutil.DebugPrint(screen, msg)
+	if matches == gameCompleteMatches {
+		msg = fmt.Sprintf(" GAME OVER ...Tries %d Matches %d", tries, matches)
+		hgameover = true
+	} else {
+		msg = fmt.Sprintf(" Tries %d Matches %d", tries, matches)
+	}
 
-    // if show {
-    //     return
-    // }
-    //fmt.Printf("MG_Draw\n")
-    //show = true
+	ebitenutil.DebugPrint(screen, msg)
+
+	// if show {
+	//     return
+	// }
+	//fmt.Printf("MG_Draw\n")
+	//show = true
 }
-
 
 /*
 generate n/2 random codes
 copy to the other half of the n array to get pairs of cards
 return array
 */
-func genColorCodes(ce [numRows*numCols]int) [numRows*numCols]int {
-    var (
-        m, j int
-    )
-    //fmt.Printf("ce %d\n", ce[0])
-    m = numCols * numRows
-    j = m / 2
-    for i := 0; i < m / 2; i++ {
-        ce[i] = rand.Intn(numColors)
-        ce[j] = ce[i]
-        j++
-    }
-    fmt.Println(ce) 
-    //ce[0] = 1965
-    return ce
+func genColorCodes(ce [numRows * numCols]int) [numRows * numCols]int {
+	var (
+		m, j int
+	)
+	//fmt.Printf("ce %d\n", ce[0])
+	m = numCols * numRows
+	j = m / 2
+	for i := 0; i < m/2; i++ {
+		ce[i] = rand.Intn(numColors)
+		ce[j] = ce[i]
+		j++
+	}
+	fmt.Println(ce)
+	//ce[0] = 1965
+	return ce
 }
 
 func MG_Init() {
-    var (
-        x float64
-        y float64
-        w float64
-        h float64
-        fudge float64
-        cc [numRows*numCols]int
-    )
-    tries = 0
-    matches = 0
-    hgameover = false
-    backToMain = 0
+	var (
+		x     float64
+		y     float64
+		w     float64
+		h     float64
+		fudge float64
+		cc    [numRows * numCols]int
+	)
+	tries = 0
+	matches = 0
+	hgameover = false
+	backToMain = 0
 
-    fmt.Printf("MG_Init\n")
-    rand.Seed(time.Now().UnixNano()) // do it once during app initialization
+	fmt.Printf("MG_Init\n")
+	rand.Seed(time.Now().UnixNano()) // do it once during app initialization
 
-    cc[0] = 37
-    cc = genColorCodes(cc)
-    fmt.Printf("cc %d\n", cc[0])
-    show = false
-    fudge = 9
-    x = oneGap + fudge
-    y = oneGap * 3
-    w = oneWidth
-    h = oneHeight
-    r := 0
-    c := 1
-    for i := 0; i < numCards; i++ {
-        cardArr = append(cardArr, aCardData)
-        cardArr[i].x = x
-        cardArr[i].y = y
-        cardArr[i].w = w
-        cardArr[i].h = h
-        cardArr[i].clicked = false
-        cardArr[i].colorCode = cc[i] //rand.Intn(numColors)
-        x = x + oneWidth + oneGap
-        c += 1
-        if c > numCols {
-            c = 1
-            r += 1
-            x = oneGap + fudge
-            y = y + oneHeight + oneGap
-        }
-    }
+	cc[0] = 37
+	cc = genColorCodes(cc)
+	fmt.Printf("cc %d\n", cc[0])
+	show = false
+	fudge = 9
+	x = oneGap + fudge
+	y = oneGap * 3
+	w = oneWidth
+	h = oneHeight
+	r := 0
+	c := 1
+	for i := 0; i < numCards; i++ {
+		cardArr = append(cardArr, aCardData)
+		cardArr[i].x = x
+		cardArr[i].y = y
+		cardArr[i].w = w
+		cardArr[i].h = h
+		cardArr[i].clicked = false
+		cardArr[i].colorCode = cc[i] //rand.Intn(numColors)
+		x = x + oneWidth + oneGap
+		c += 1
+		if c > numCols {
+			c = 1
+			r += 1
+			x = oneGap + fudge
+			y = y + oneHeight + oneGap
+		}
+	}
 
-    if cardRectImage == nil {
-        // Create an w x h image
-        cardRectImage, _ = ebiten.NewImage(oneWidth, oneHeight, ebiten.FilterNearest)
-    } 
+	if cardRectImage == nil {
+		// Create an w x h image
+		cardRectImage, _ = ebiten.NewImage(oneWidth, oneHeight, ebiten.FilterNearest)
+	}
 
-    // Fill the square with the white color
-    //cardRectImage.Fill(color.White)
-    orange := color.NRGBA{255, 128, 0, 0xff}
-    cardRectImage.Fill(orange)
+	// Fill the square with the white color
+	//cardRectImage.Fill(color.White)
+	orange := color.NRGBA{255, 128, 0, 0xff}
+	cardRectImage.Fill(orange)
 
-    if frontCardImage == nil {
-        // Create an w x h image
-        frontCardImage, _ = ebiten.NewImage(oneWidth, oneHeight, ebiten.FilterNearest)
-    } 
-    green := color.NRGBA{0, 128, 0, 0xff}
-    frontCardImage.Fill(green)
+	if frontCardImage == nil {
+		// Create an w x h image
+		frontCardImage, _ = ebiten.NewImage(oneWidth, oneHeight, ebiten.FilterNearest)
+	}
+	green := color.NRGBA{0, 128, 0, 0xff}
+	frontCardImage.Fill(green)
 
-    show_all()
-    frameCount = 0
-    timerLockShowAll = false
-    ShowTwoTimerLock = true
-    initMatcher()
+	show_all()
+	frameCount = 0
+	timerLockShowAll = false
+	ShowTwoTimerLock = true
+	initMatcher()
 
-
-    rand.Seed(time.Now().UnixNano()) // do it once during app initialization
-    s := []int{1, 2, 3, 4, 5}
-    Shuffle(s)
-    fmt.Println(s) // Example output: [4 3 2 1 5]
+	rand.Seed(time.Now().UnixNano()) // do it once during app initialization
+	s := []int{1, 2, 3, 4, 5}
+	Shuffle(s)
+	fmt.Println(s) // Example output: [4 3 2 1 5]
 }
-
 
 /*
 from
 https://stackoverflow.com/questions/12264789/shuffle-array-in-go
 */
 func Shuffle(slice interface{}) {
-    rv := reflect.ValueOf(slice)
-    swap := reflect.Swapper(slice)
-    length := rv.Len()
-    for i := length - 1; i > 0; i-- {
-        j := rand.Intn(i + 1)
-        swap(i, j)
-    }
+	rv := reflect.ValueOf(slice)
+	swap := reflect.Swapper(slice)
+	length := rv.Len()
+	for i := length - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		swap(i, j)
+	}
 }
-
-
-
-
--- memoryGameMain.go
+++ memoryGameMain.go
@@ -1,5 +1,3 @@
-
-
 /*
 
 
@@ -14,84 +12,80 @@ https://github.com/hajimehoshi/ebiten/wiki/Tutorial%3AScreen%2C-colors-and-squar
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "path/filepath"
-    "image"
-    "image/color"
-    "github.com/hajimehoshi/ebiten"
-    "github.com/hajimehoshi/ebiten/audio"
-    "github.com/hajimehoshi/ebiten/audio/wav"
-    "github.com/hajimehoshi/ebiten/ebitenutil"
-    //"github.com/hajimehoshi/ebiten/keys"
-
+	"fmt"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/audio"
+	"github.com/hajimehoshi/ebiten/audio/wav"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"path/filepath"
+	//"github.com/hajimehoshi/ebiten/keys"
 )
 
 var (
-  	square *ebiten.Image
-  	x,y float64
-  	mode int
-    audioContext *audio.Context
-    audioPlayer  *audio.Player
-    delayCount int
-    prevkey string
-    digitsImage *ebiten.Image
-    mouseReleased bool
-    gameover bool
-    buttonRunTimer buttonTimerStruct
-    buttonQuitTimer buttonTimerStruct
-
-
+	square          *ebiten.Image
+	x, y            float64
+	mode            int
+	audioContext    *audio.Context
+	audioPlayer     *audio.Player
+	delayCount      int
+	prevkey         string
+	digitsImage     *ebiten.Image
+	mouseReleased   bool
+	gameover        bool
+	buttonRunTimer  buttonTimerStruct
+	buttonQuitTimer buttonTimerStruct
 )
 
-const (   // const
-  screenwidth = 320
-  screenheight = 320
-  modeStartScreen = 1
-  modeGameScreen = 2
-  sampleRate   = 44100
-  startButtonCode = 801
-  quitButtonCode = 802
+const ( // const
+	screenwidth     = 320
+	screenheight    = 320
+	modeStartScreen = 1
+	modeGameScreen  = 2
+	sampleRate      = 44100
+	startButtonCode = 801
+	quitButtonCode  = 802
 )
 
 func soundinit() {
-    var err error
-    audioContext, err = audio.NewContext(sampleRate)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var err error
+	audioContext, err = audio.NewContext(sampleRate)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    f, err := os.Open(filepath.Join("data", "jab.wav"))
-    if err != nil {
-        log.Fatal(err)
-    }
+	f, err := os.Open(filepath.Join("data", "jab.wav"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    d, err := wav.Decode(audioContext, f)
-    if err != nil {
-        log.Fatal(err)
-    }
+	d, err := wav.Decode(audioContext, f)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    audioPlayer, err = audio.NewPlayer(audioContext, d)
-    if err != nil {
-        log.Fatal(err)
-    }
+	audioPlayer, err = audio.NewPlayer(audioContext, d)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func exitProgram() {
-    os.Exit(0)
+	os.Exit(0)
 }
 
 func digitsDraw(screen *ebiten.Image) {
-    
-    opts := &ebiten.DrawImageOptions{}
+
+	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Reset()
 	opts.GeoM.Translate(10, 10)
 	r := image.Rect(50, 0, 100, 80)
 	opts.SourceRect = &r
-    screen.DrawImage(digitsImage, opts)
+	screen.DrawImage(digitsImage, opts)
 }
-
 
 /*
 check if same function name can exist in the same project
@@ -100,68 +94,65 @@ func Haha1() {
 	fmt.Print("Ha ha ebitut\n")
 }
 
-
 // where you choose to begin the game or exit
 func outerloop(screen *ebiten.Image) error {
 
-    //screen.Fill(color.NRGBA{0xff, 0xff, 128, 0xff})     // 
-    screen.Fill(color.NRGBA{154, 158, 5, 0xff}) 
+	//screen.Fill(color.NRGBA{0xff, 0xff, 128, 0xff})     //
+	screen.Fill(color.NRGBA{154, 158, 5, 0xff})
 
-    if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) &&
-        mouseReleased {
-        mx, my := ebiten.CursorPosition()
-        mouseReleased = false
-        if Button_Click(mx,my) == startButtonCode {
-            fmt.Printf("click button\n")
-            //mode = modeGameScreen
-            //MG_Init()
-            buttonRunTimer.set(buttonRunTimer.kButtonStart,30) // 60 is 1 seconds\
-        
-        } else if Button_Click(mx,my) == quitButtonCode {
-            buttonQuitTimer.set(buttonRunTimer.kButtonStart,30)
-        }
-    } else if ! ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-        mouseReleased = true
-    }
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) &&
+		mouseReleased {
+		mx, my := ebiten.CursorPosition()
+		mouseReleased = false
+		if Button_Click(mx, my) == startButtonCode {
+			fmt.Printf("click button\n")
+			//mode = modeGameScreen
+			//MG_Init()
+			buttonRunTimer.set(buttonRunTimer.kButtonStart, 30) // 60 is 1 seconds\
 
+		} else if Button_Click(mx, my) == quitButtonCode {
+			buttonQuitTimer.set(buttonRunTimer.kButtonStart, 30)
+		}
+	} else if !ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		mouseReleased = true
+	}
 
-    if ebiten.IsKeyPressed(ebiten.KeyQ) {
-    	exitProgram()
-    }
-    
-    Button_Draw(screen)
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		exitProgram()
+	}
 
-    // timers are to allow the user to see the button press animation
-    buttonRunTimer.inc()
-    if buttonRunTimer.check() {
-        fmt.Printf("buttonRunTimer execcute!\n")
-        buttonRunTimer.reset()
-        mode = modeGameScreen
-        MG_Init()
-  
-    }
+	Button_Draw(screen)
 
-    buttonQuitTimer.inc()
-    if buttonQuitTimer.check() {
-        fmt.Printf("quit execcute!\n")
-        buttonQuitTimer.reset()  // unnecessary but for consistency
-        exitProgram()
-    }
+	// timers are to allow the user to see the button press animation
+	buttonRunTimer.inc()
+	if buttonRunTimer.check() {
+		fmt.Printf("buttonRunTimer execcute!\n")
+		buttonRunTimer.reset()
+		mode = modeGameScreen
+		MG_Init()
+
+	}
+
+	buttonQuitTimer.inc()
+	if buttonQuitTimer.check() {
+		fmt.Printf("quit execcute!\n")
+		buttonQuitTimer.reset() // unnecessary but for consistency
+		exitProgram()
+	}
 
 	//digitsDraw(screen)
 
-    if ebiten.IsKeyPressed(ebiten.KeyJ) && PushKeyOnce("J") {
-       fmt.Print("key J\n")
-       //PushKeyOnce("J")
-    }
-    if ebiten.IsKeyPressed(ebiten.KeyU) && PushKeyOnce("U") {
-       fmt.Print("key U\n")
-       //PushKeyOnce("U")
-    }
-    ebitenutil.DebugPrint(screen, "Memory Game \nin Ebiten\n")
-    return nil
+	if ebiten.IsKeyPressed(ebiten.KeyJ) && PushKeyOnce("J") {
+		fmt.Print("key J\n")
+		//PushKeyOnce("J")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyU) && PushKeyOnce("U") {
+		fmt.Print("key U\n")
+		//PushKeyOnce("U")
+	}
+	ebitenutil.DebugPrint(screen, "Memory Game \nin Ebiten\n")
+	return nil
 }
-
 
 /*
 
@@ -170,12 +161,12 @@ need to store a map of which keys need to be non-repeating
 */
 
 func PushKeyOnce(k string) bool {
-     if k != prevkey {
-     	 prevkey = k
-	 return true
-     }
-     prevkey = k
-     return false
+	if k != prevkey {
+		prevkey = k
+		return true
+	}
+	prevkey = k
+	return false
 }
 
 // actual game
@@ -187,46 +178,40 @@ func innerloop(screen *ebiten.Image) error {
 		return nil
 	}
 
-
-    if ebiten.IsKeyPressed(ebiten.KeyP) && !audioPlayer.IsPlaying() {
-        audioPlayer.Rewind()
-        audioPlayer.Play()
-    }
-
-    if err := audioContext.Update(); err != nil {
-        return err
+	if ebiten.IsKeyPressed(ebiten.KeyP) && !audioPlayer.IsPlaying() {
+		audioPlayer.Rewind()
+		audioPlayer.Play()
 	}
 
+	if err := audioContext.Update(); err != nil {
+		return err
+	}
 
-    // Fill the screen with #FF0000 color
-    //screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})   // red
-    //screen.Fill(color.NRGBA{0xaa, 0xff, 0xff, 0xff})     // green
-    screen.Fill(color.NRGBA{0x00, 0x00, 0x00, 0xff}) //black
-    MG_Draw(screen)
+	// Fill the screen with #FF0000 color
+	//screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})   // red
+	//screen.Fill(color.NRGBA{0xaa, 0xff, 0xff, 0xff})     // green
+	screen.Fill(color.NRGBA{0x00, 0x00, 0x00, 0xff}) //black
+	MG_Draw(screen)
 
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) &&
+		mouseReleased {
+		mx, my := ebiten.CursorPosition()
+		gameover = mG_Click(mx, my)
+		mouseReleased = false
+		fmt.Printf("mouseRele false\n")
+	} else if !ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		//fmt.Printf("mouseReleased = true\n")
+		mouseReleased = true
+	}
 
-    if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) &&
-        mouseReleased {
-        mx, my := ebiten.CursorPosition()
-        gameover = mG_Click(mx, my)
-        mouseReleased = false
-        fmt.Printf("mouseRele false\n")
-    } else if ! ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-        //fmt.Printf("mouseReleased = true\n")
-        mouseReleased = true
-    }
+	//if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+	if gameover || ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		//mode = modeStartScreen
+		gameinit()
+		ebiten.SetFullscreen(false)
+	}
 
-
-    
-    //if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-    if gameover || ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
-    	//mode = modeStartScreen
-    	gameinit()
-    	ebiten.SetFullscreen(false)
-    }
-
-
-    return nil
+	return nil
 }
 
 func update(screen *ebiten.Image) error {
@@ -239,43 +224,42 @@ func update(screen *ebiten.Image) error {
 
 	}
 
-    return nil
+	return nil
 }
 
 func gameinit() {
 
-     mode = modeStartScreen
-     x = 0
-     y = 25
-     delayCount = 0
-     mouseReleased = true
-     gameover = false
+	mode = modeStartScreen
+	x = 0
+	y = 25
+	delayCount = 0
+	mouseReleased = true
+	gameover = false
 }
 
 func runOnce() {
-    fmt.Printf("running ********************************\n")
+	fmt.Printf("running ********************************\n")
 
-    buttonOOP.init()
-    b1 := new(buttonobjectData)
-    b1.name = "Jack"
-    b2 := new(buttonobjectData)
-    b2.name = "Joey"
-    b1.init()
-    b2.init()
+	buttonOOP.init()
+	b1 := new(buttonobjectData)
+	b1.name = "Jack"
+	b2 := new(buttonobjectData)
+	b2.name = "Joey"
+	b1.init()
+	b2.init()
 
+	Button_Init()
+	Button_Create("run.png", startButtonCode, 100, 100, 200, 100, true)
+	Button_Create("quit.png", quitButtonCode, 100, 220, 200, 100, true)
+	buttonRunTimer.reset()
+	buttonQuitTimer.reset()
 
-    Button_Init()
-    Button_Create("run.png",startButtonCode,100,100,200,100,true)
-    Button_Create("quit.png",quitButtonCode,100,220,200,100,true)
-    buttonRunTimer.reset()
-    buttonQuitTimer.reset()
-    
-    var err error
-    var fname string
+	var err error
+	var fname string
 	fname = filepath.Join("data", "digits.png")
 	digitsImage, _, err = ebitenutil.NewImageFromFile(
-        fname, 
-        ebiten.FilterNearest)
+		fname,
+		ebiten.FilterNearest)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -283,18 +267,17 @@ func runOnce() {
 }
 
 func main() {
-    runOnce()
-    gameinit()
-    soundinit()
-    ebiten.SetRunnableInBackground(true)
-    ebiten.SetFullscreen(false)
-    
-    scale := 2.0  
-    // Initialize Ebiten, and loop the update() function
-    if err := ebiten.Run(update, screenwidth, screenheight, scale, "Memory Game 0.3 by George Loo"); err != nil {
-        panic(err)
-    }
-    fmt.Printf("Program ended \n")
-    
-}
+	runOnce()
+	gameinit()
+	soundinit()
+	ebiten.SetRunnableInBackground(true)
+	ebiten.SetFullscreen(false)
 
+	scale := 2.0
+	// Initialize Ebiten, and loop the update() function
+	if err := ebiten.Run(update, screenwidth, screenheight, scale, "Memory Game 0.3 by George Loo"); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Program ended \n")
+
+}
